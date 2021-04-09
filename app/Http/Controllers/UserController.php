af53f5ddc8c970e2b2c0890e75669a2cfbd57c70
raksmala <raksmala@gmail.com>
2021-04-09 10:09:37 +0700

    tes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,21 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\UKM;
-use DB;
 
 class UserController extends Controller
 {
     public function index() 
     {
-        // $ukm = UKM::where([['idUKM', '!=', '1']])->get();
-        try{
-            DB::Connection()->getPdo();
-            $ukm = DB::select('select * from ukm where idUKM != 1');
-            print_r($ukm);
-            die('Terhubung');
-        } catch (\Exception $e) {
-            die('Tidak terhubung dengan database. Error:'.$e);
-        }
+        $ukm = UKM::where([['idUKM', '!=', '1']])->get();
         return view('/user/user', ['ukm' => $ukm]);
     }
 }
