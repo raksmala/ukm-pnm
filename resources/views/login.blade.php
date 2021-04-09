c5addf4605baab7762f961103b3dae727e5ea929
raksmala <raksmala@gmail.com>
2021-04-09 18:57:32 +0700

    Tes 3

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="description" content="A fully featured admin theme which can be used to build CRM, CMS, etc.">
+        <meta name="description" content="Unit Kegiatan Mahasiswa Politeknik Negeri Madiun">
         <meta name="author" content="Coderthemes">
 
         <link rel="shortcut icon" href="{{ secure_asset('/assets/images/logo/km.png') }}">
