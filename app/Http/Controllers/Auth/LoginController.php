cbe16f0bbc47bc14fcf112d1a43692356b80f302
raksmala <raksmala@gmail.com>
2021-04-09 13:02:15 +0700

    Penambahan revisi pada redirect secure folder middleware

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -29,7 +29,7 @@ class LoginController extends Controller
      */
     protected function redirectTo()
     {
-        return redirect()->secure('/admin');
+        return '/admin';
     }
 
     /**
