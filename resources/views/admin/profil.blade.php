c8e9534d9256b1781b7611b940d22d7ba7e61274
unknown <raksmala@gmail.com>
2021-06-04 14:39:49 +0700

    UI Halaman Profil Admin UKM

--- resources/views/admin/profil.blade.php
+++ resources/views/admin/profil.blade.php
@@ -10,36 +10,180 @@
 <div class="row">
     <div class="col-md-4 col-lg-3">
         <div class="profile-detail card-box">
-            <div>
+            <div style="text-align: center;">
                 <img src="{{ secure_url('') }}/assets/images/logo/{{ Auth()->user()->foto }}" class="img-circle" alt="profile-image">
                 <button type="button" class="btn btn-pink btn-custom btn-rounded waves-effect waves-light">Follow</button>
             </div>
         </div>
     </div>
-    <div class="col-lg-9 col-md-8">
-        <form method="post" class="well">
-            <span class="input-icon icon-right">
-                <textarea rows="2" class="form-control"
-                            placeholder="Post a new message"></textarea>
-            </span>
-            <div class="p-t-10 pull-right">
-                <a class="btn btn-sm btn-primary waves-effect waves-light">Send</a>
+    <div class="card-box">
+        <div class="comment">
+            <img src="assets/images/users/avatar-1.jpg" alt="" class="comment-avatar">
+            <div class="comment-body">
+                <div class="comment-text">
+                    <div class="comment-header">
+                        <a href="#" title="">Adam Jansen</a><span>about 2 minuts ago</span>
+                    </div>
+                    Story based around the idea of time lapse, animation to post soon!
+
+                    <div class="m-t-15">
+                        <a href="">
+                            <img src="assets/images/small/img1.jpg" class="thumb-md">
+                        </a>
+                        <a href="">
+                            <img src="assets/images/small/img2.jpg" class="thumb-md">
+                        </a>
+                        <a href="">
+                            <img src="assets/images/small/img3.jpg" class="thumb-md">
+                        </a>
+                    </div>
+                </div>
+
+                <div class="comment-footer">
+                    <a href="#"><i class="fa fa-thumbs-o-up"></i></a>
+                    <a href="#"><i class="fa fa-thumbs-o-down"></i></a>
+                    <a href="#">Reply</a>
+                </div>
             </div>
-            <ul class="nav nav-pills profile-pills m-t-10">
-                <li>
-                    <a href="#"><i class="fa fa-user"></i></a>
-                </li>
-                <li>
-                    <a href="#"><i class="fa fa-location-arrow"></i></a>
-                </li>
-                <li>
-                    <a href="#"><i class=" fa fa-camera"></i></a>
-                </li>
-                <li>
-                    <a href="#"><i class="fa fa-smile-o"></i></a>
-                </li>
-            </ul>
-        </form>
+
+            <div class="comment">
+                <img src="assets/images/users/avatar-2.jpg" alt="" class="comment-avatar">
+                <div class="comment-body">
+                    <div class="comment-text">
+                        <div class="comment-header">
+                            <a href="#" title="">John Smith</a><span>about 1 hour ago</span>
+                        </div>
+                        Wow impressive!
+                    </div>
+                    <div class="comment-footer">
+                        <a href="#"><i class="fa fa-thumbs-o-up"></i></a>
+                        <a href="#"><i class="fa fa-thumbs-o-down"></i></a>
+                        <a href="#">Reply</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="comment">
+                <img src="assets/images/users/avatar-3.jpg" alt=""
+                        class="comment-avatar">
+                <div class="comment-body">
+                    <div class="comment-text">
+                        <div class="comment-header">
+                            <a href="#" title="">Matt
+                                Cheuvront</a><span>about 2 hours ago</span>
+                        </div>
+                        Wow, that is really nice.
+                    </div>
+                    <div class="comment-footer">
+                        <a href="#"><i class="fa fa-thumbs-o-up"></i></a>
+                        <a href="#"><i class="fa fa-thumbs-o-down"></i></a>
+                        <a href="#">Reply</a>
+                    </div>
+                </div>
+
+                <div class="comment">
+                    <img src="assets/images/users/avatar-4.jpg" alt=""
+                            class="comment-avatar">
+                    <div class="comment-body">
+                        <div class="comment-text">
+                            <div class="comment-header">
+                                <a href="#" title="">Stephanie
+                                    Walter</a><span>3 hours ago</span>
+                            </div>
+                            Nice work, makes me think of The Money Pit.
+                        </div>
+                        <div class="comment-footer">
+                            <a href="#"><i class="fa fa-thumbs-o-up"></i></a>
+                            <a href="#"><i class="fa fa-thumbs-o-down"></i></a>
+                            <a href="#">Reply</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="comment">
+            <img src="assets/images/users/avatar-1.jpg" alt="" class="comment-avatar">
+            <div class="comment-body">
+                <div class="comment-text">
+                    <div class="comment-header">
+                        <a href="#" title="">Kim Ryder</a><span>about 4 hours ago</span>
+                    </div>
+                    i'm in the middle of a timelapse animation myself! (Very different
+                    though.) Awesome stuff.
+                </div>
+                <div class="comment-footer">
+                    <a href="#"><i class="fa fa-thumbs-o-up"></i></a>
+                    <a href="#"><i class="fa fa-thumbs-o-down"></i></a>
+                    <a href="#">Reply</a>
+                </div>
+            </div>
+        </div>
+        <div class="comment">
+            <img src="assets/images/users/avatar-7.jpg" alt="" class="comment-avatar">
+            <div class="comment-body">
+                <div class="comment-text">
+                    <div class="comment-header">
+                        <a href="#" title="">Nicolai Larson</a><span>10 hours ago</span>
+                    </div>
+                    the parallax is a little odd but O.o that house build is awesome!!
+                </div>
+                <div class="comment-footer">
+                    <a href="#"><i class="fa fa-thumbs-o-up"></i></a>
+                    <a href="#"><i class="fa fa-thumbs-o-down"></i></a>
+                    <a href="#">Reply</a>
+                </div>
+            </div>
+        </div>
+        <div class="comment">
+            <img src="assets/images/users/avatar-1.jpg" alt="" class="comment-avatar">
+            <div class="comment-body">
+                <div class="comment-text">
+                    <div class="comment-header">
+                        <a href="#" title="">Adam Jansen</a><span>about 2 minuts ago</span>
+                    </div>
+                    Story based around the idea of time lapse, animation to post soon!
+
+                    <div class="m-t-15">
+                        <a href="">
+                            <img src="assets/images/small/img1.jpg" class="thumb-md">
+                        </a>
+                        <a href="">
+                            <img src="assets/images/small/img2.jpg" class="thumb-md">
+                        </a>
+                        <a href="">
+                            <img src="assets/images/small/img3.jpg" class="thumb-md">
+                        </a>
+                    </div>
+                </div>
+
+                <div class="comment-footer">
+                    <a href="#"><i class="fa fa-thumbs-o-up"></i></a>
+                    <a href="#"><i class="fa fa-thumbs-o-down"></i></a>
+                    <a href="#">Reply</a>
+                </div>
+            </div>
+
+            <div class="comment">
+                <img src="assets/images/users/avatar-2.jpg" alt="" class="comment-avatar">
+                <div class="comment-body">
+                    <div class="comment-text">
+                        <div class="comment-header">
+                            <a href="#" title="">John Smith</a><span>about 1 hour ago</span>
+                        </div>
+                        Wow impressive!
+                    </div>
+                    <div class="comment-footer">
+                        <a href="#"><i class="fa fa-thumbs-o-up"></i></a>
+                        <a href="#"><i class="fa fa-thumbs-o-down"></i></a>
+                        <a href="#">Reply</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="m-t-30 text-center">
+            <a href="" class="btn btn-default waves-effect waves-light btn-sm">Load More...</a>
+        </div>
     </div>
 </div>
 @endsection
