74d75c4961f81fff901f176492ac05a553a46dda
unknown <raksmala@gmail.com>
2021-06-06 10:00:37 +0700

    Backend Profil Admin UKM

--- app/Http/Controllers/Admin/LaporanController.php
+++ app/Http/Controllers/Admin/LaporanController.php
@@ -22,6 +22,8 @@ class LaporanController extends Controller
     	]);
 
         $foto = $request->file("fotoKegiatanProker$idProgramKerja");
+        var_dump($foto);
+        die;
         $namaFoto = Auth()->user()->UKM_idUKM.'-'.$idProgramKerja.'.'.$foto->getClientOriginalExtension();
         $pathUpload = 'upload/';
 
