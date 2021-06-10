60d51d618242d83f4d35d79d2a4953ca42562809
unknown <raksmala@gmail.com>
2021-06-11 00:08:59 +0700

    BEM Detail Jadwal

--- app/Http/Controllers/BEM/JadwalController.php
+++ app/Http/Controllers/BEM/JadwalController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\BEM;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Jadwal;
+use App\DetailJadwal;
 
 class JadwalController extends Controller
 {
