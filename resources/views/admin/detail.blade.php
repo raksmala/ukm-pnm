ad7fcfb73fa2d7ad20ace762c7c1c8ba39631f27
unknown <raksmala@gmail.com>
2021-06-20 15:35:55 +0700

    Menghapus tombol tambah pada halaman detail jadwal

--- resources/views/admin/detail.blade.php
+++ resources/views/admin/detail.blade.php
@@ -10,9 +10,6 @@
 
 <div class="col-lg-12">
     <div class="card-box table-responsive">
-        <div style="width: 100%; text-align: right; margin-bottom: 10px;">
-            <a href="#" class="on-default edit-row btn btn-success" data-toggle="modal" data-target="#tambah-modal"><i class="fa fa-plus"></i></a>
-        </div>
 
         <table id="datatable" class="table table-striped table-bordered">
             <thead>
